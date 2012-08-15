0d00f4b4a38a818d4945d58b62f543fceb2e438d
Bajee11 <bajee11@gmail.com>
2012-08-15 00:38:37 -0400

    Apply proper date format Fix date display for OTA Updater

--- configs/common_versions.mk
+++ configs/common_versions.mk
@@ -1,14 +1,16 @@
 # Version information used on all builds
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_DISPLAY_ID=RootBox-JRO03H BUILD_VERSION_TAGS=release-keys USER=android-build BUILD_UTC_DATE=$(shell date +"%s")
 
+DATE = $(shell date +%Y%m%d-%H%M)
+
 # Rom Manager properties
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.aokp.version=V2.2-JB
+    ro.aokp.version=V2.2-I9100
 
 # RootBox OTA Updater
 PRODUCT_PROPERTY_OVERRIDES += \
     otaupdater.otaid=bajee11 \
-    otaupdater.otatime=$(date +"%Y%m%d"-"%H%M") \
+    otaupdater.otatime=$(DATE) \
     otaupdater.otaver=2.2
 
 # Camera shutter sound property
