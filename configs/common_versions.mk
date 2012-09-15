6b42e0a45062b5027bbd41f24bb9a94bcd062bb3
Bajee11 <bajee11@gmail.com>
2012-09-15 00:53:49 -0400

    Update to RootBox v2.5

--- configs/common_versions.mk
+++ configs/common_versions.mk
@@ -5,7 +5,7 @@ DATE = $(shell date +%Y%m%d-%H%M)
 
 # Rom Manager properties
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.aokp.version=RootBox_JB_V2.4
+    ro.aokp.version=RootBox_JB_V2.5
 
 
 # Camera shutter sound property
