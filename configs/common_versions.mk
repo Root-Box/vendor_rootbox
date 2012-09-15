9e74e75afcd7eb86157c2880f157e1487182bf7e
Bajee11 <bajee11@gmail.com>
2012-09-15 11:59:57 -0400

    Modify some Goo updater infos

--- configs/common_versions.mk
+++ configs/common_versions.mk
@@ -9,10 +9,9 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Goo updater app
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.goo.board=$(TARGET_PRODUCT) \
     ro.goo.developerid=Bajee \
     ro.goo.rom=RootBox \
-    ro.goo.version=2.5
+    ro.goo.version=250
 
 
 # Camera shutter sound property
