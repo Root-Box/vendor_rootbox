dd217b9949b6cbfd3ca0f70cf6e99130979e6aae
Bajee11 <bajee11@gmail.com>
2012-09-20 21:24:26 -0400

    Upgrade RootBox V2.6

--- configs/common_versions.mk
+++ configs/common_versions.mk
@@ -5,13 +5,13 @@ DATE = $(shell date +%Y%m%d-%H%M)
 
 # Rom Manager properties
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.aokp.version=RootBox-JB-V2.5
+    ro.aokp.version=RootBox-JB-V2.6
 
 # Goo updater app
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.goo.developerid=Bajee \
     ro.goo.rom=RootBox \
-    ro.goo.version=250
+    ro.goo.version=260
 
 
 # Camera shutter sound property
