462af9dc8f83522fe9b7ffa3f93074e4f01852ac
Bajee11 <bajee11@gmail.com>
2012-10-15 23:43:15 -0400

    Fix configs path

--- products/d2att.mk
+++ products/d2att.mk
@@ -2,10 +2,10 @@
 $(call inherit-product, device/samsung/d2att/full_d2att.mk)
 
 # Inherit common product files.
-$(call inherit-product, vendor/aokp/config/common_phone.mk)
+$(call inherit-product, vendor/aokp/configs/common_phone.mk)
 
 # Inherit GSM common stuff.
-$(call inherit-product, vendor/aokp/config/gsm.mk)
+$(call inherit-product, vendor/aokp/configs/gsm.mk)
 
 PRODUCT_PACKAGE_OVERLAYS += vendor/aokp/overlay/d2-common
 
