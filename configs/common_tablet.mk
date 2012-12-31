3a7403eaee394d04502c4e801f4c5226bd23b52a
Bajee11 <bajee11@gmail.com>
2012-12-31 13:16:27 -0500

    Add grouper support and cleanup tablet configs.

--- configs/common_tablet.mk
+++ configs/common_tablet.mk
@@ -2,13 +2,3 @@
 $(call inherit-product, vendor/rootbox/configs/common.mk)
 
 PRODUCT_PACKAGE_OVERLAYS += vendor/rootbox/overlay/common_tablets
-
-PRODUCT_PACKAGES += \
-    Trebuchet
-
-# Inherit drm blobs
--include vendor/rootbox/configs/common_drm_tablet.mk
-
-# BT config
-PRODUCT_COPY_FILES += \
-    system/bluetooth/data/main.nonsmartphone.conf:system/etc/bluetooth/main.conf
---
+++ products/grouper.mk
@@ -0,0 +1,26 @@
+# Inherit AOSP device configuration for grouper
+$(call inherit-product, device/asus/grouper/full_grouper.mk)
+
+# Inherit AOKP common_tablet bits
+$(call inherit-product, vendor/rootbox/configs/common_tablet.mk)
+
+# Grouper Overlay
+PRODUCT_PACKAGE_OVERLAYS += vendor/rootbox/overlay/grouper
+
+# Setup device specific product configuration.
+PRODUCT_NAME := rootbox_grouper
+PRODUCT_BRAND := google
+PRODUCT_DEVICE := grouper
+PRODUCT_MODEL := Nexus 7
+PRODUCT_MANUFACTURER := asus
+
+PRODUCT_BUILD_PROP_OVERRIDES := PRODUCT_NAME=nakasi BUILD_FINGERPRINT=google/nakasi/grouper:4.1.1/JRO03H/405518:user/release-keys PRIVATE_BUILD_DESC="nakasi-user 4.1.1 JRO03H 405518 release-keys" BUILD_NUMBER=405518
+
+PRODUCT_COPY_FILES += \
+    vendor/rootbox/prebuilt/xhdpi/bootanimation.zip:system/media/bootanimation.zip
+
+# Copy Onandroid partitions table
+PRODUCT_COPY_FILES += \
+    vendor/rootbox/prebuilt/common/onandroid/grouper/partlayout4nandroid:system/partlayout4nandroid
+
+
