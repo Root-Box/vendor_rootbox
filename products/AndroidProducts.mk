667e25ac5c09827928849bf0b902256245273e17
Bajee11 <bajee11@gmail.com>
2013-02-02 03:34:32 -0500

    Add l900/i605 support

--- products/AndroidProducts.mk
+++ products/AndroidProducts.mk
@@ -2,10 +2,12 @@ PRODUCT_MAKEFILES := $(LOCAL_DIR)/d2att.mk \
 $(LOCAL_DIR)/d2tmo.mk \
 $(LOCAL_DIR)/d2vzw.mk \
 $(LOCAL_DIR)/grouper.mk \
+$(LOCAL_DIR)/i605.mk \
 $(LOCAL_DIR)/i9100.mk \
 $(LOCAL_DIR)/i9100g.mk \
 $(LOCAL_DIR)/i9100p.mk \
 $(LOCAL_DIR)/i9300.mk \
+$(LOCAL_DIR)/l900.mk \
 $(LOCAL_DIR)/mako.mk \
 $(LOCAL_DIR)/n7000.mk \
 $(LOCAL_DIR)/n7100.mk
---
+++ products/l900.mk
@@ -0,0 +1,26 @@
+# Inherit AOSP device configuration for l900.
+$(call inherit-product, device/samsung/l900/full_l900.mk)
+
+# Inherit CDMA common stuff.
+$(call inherit-product, vendor/rootbox/configs/cdma.mk)
+
+# Inherit AOKP common bits
+$(call inherit-product, vendor/rootbox/configs/common.mk)
+
+# t0ltecdma Overlays
+PRODUCT_PACKAGE_OVERLAYS += vendor/rootbox/overlay/t0ltecdma
+
+# Setup device specific product configuration.
+PRODUCT_NAME := rootbox_l900
+PRODUCT_BRAND := Samsung
+PRODUCT_DEVICE := l900
+PRODUCT_MODEL := SPH-L900
+PRODUCT_MANUFACTURER := Samsung
+
+# Set build fingerprint / ID / Product Name ect.
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=t0ltespr TARGET_DEVICE=t0ltespr BUILD_FINGERPRINT="samsung/t0ltespr/t0ltespr:4.1.1/JRO03C/L900VPALJC:user/release-keys" PRIVATE_BUILD_DESC="t0ltespr-user 4.1.1 JRO03C L900VPALJC release-keys"
+
+# Copy bootanimation.zip
+PRODUCT_COPY_FILES += \
+    vendor/rootbox/prebuilt/xhdpi/bootanimation.zip:system/media/bootanimation.zip
+
