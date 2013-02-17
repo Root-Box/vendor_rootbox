cd8632f53f8309dfcc9fe047112f7bf1aa4033bd
Bajee11 <bajee11@gmail.com>
2013-02-17 14:25:21 -0500

    Add Maguro Support (1/2)

--- products/AndroidProducts.mk
+++ products/AndroidProducts.mk
@@ -8,6 +8,7 @@ $(LOCAL_DIR)/i9100g.mk \
 $(LOCAL_DIR)/i9100p.mk \
 $(LOCAL_DIR)/i9300.mk \
 $(LOCAL_DIR)/l900.mk \
+$(LOCAL_DIR)/maguro.mk \
 $(LOCAL_DIR)/mako.mk \
 $(LOCAL_DIR)/n7000.mk \
 $(LOCAL_DIR)/n7100.mk
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -8,6 +8,7 @@ add_lunch_combo rootbox_i9100g-userdebug
 add_lunch_combo rootbox_i9100p-userdebug
 add_lunch_combo rootbox_i9300-userdebug
 add_lunch_combo rootbox_l900-userdebug
+add_lunch_combo rootbox_maguro-userdebug
 add_lunch_combo rootbox_mako-userdebug
 add_lunch_combo rootbox_n7000-userdebug
 add_lunch_combo rootbox_n7100-userdebug
