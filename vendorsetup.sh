f231882af13c75fc3a88c257a28e851c7b671ffb
Bajee11 <bajee11@gmail.com>
2013-03-05 20:02:24 -0500

    Add t0lte and clean up vendor

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,10 +5,10 @@ add_lunch_combo rootbox_grouper-userdebug
 add_lunch_combo rootbox_i605-userdebug
 add_lunch_combo rootbox_i9100-userdebug
 add_lunch_combo rootbox_i9100g-userdebug
-add_lunch_combo rootbox_i9100p-userdebug
 add_lunch_combo rootbox_i9300-userdebug
 add_lunch_combo rootbox_l900-userdebug
 add_lunch_combo rootbox_maguro-userdebug
 add_lunch_combo rootbox_mako-userdebug
 add_lunch_combo rootbox_n7000-userdebug
 add_lunch_combo rootbox_n7100-userdebug
+add_lunch_combo rootbox_t0lte-userdebug
