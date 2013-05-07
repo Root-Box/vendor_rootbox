8a0bca0c4e63f1a398630d0bf8b4fd60569238f7
Bajee11 <bajee11@gmail.com>
2013-05-06 23:27:05 -0400

    Add d2spr support (SPH-L710)

--- products/AndroidProducts.mk
+++ products/AndroidProducts.mk
@@ -1,4 +1,5 @@
 PRODUCT_MAKEFILES := $(LOCAL_DIR)/d2att.mk \
+$(LOCAL_DIR)/d2spr.mk \
 $(LOCAL_DIR)/d2tmo.mk \
 $(LOCAL_DIR)/d2vzw.mk \
 $(LOCAL_DIR)/grouper.mk \
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,4 +1,5 @@
 add_lunch_combo rootbox_d2att-userdebug
+add_lunch_combo rootbox_d2spr-userdebug
 add_lunch_combo rootbox_d2tmo-userdebug
 add_lunch_combo rootbox_d2vzw-userdebug
 add_lunch_combo rootbox_grouper-userdebug
