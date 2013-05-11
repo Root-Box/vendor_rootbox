c7d8629b1108f7b0da08ea223b67b08b91e600ce
apascual89 <apascual89@gmail.com>
2013-05-10 20:38:58 -0400

    Manta support

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -12,6 +12,7 @@ add_lunch_combo rootbox_jfltecan-userdebug
 add_lunch_combo rootbox_l900-userdebug
 add_lunch_combo rootbox_maguro-userdebug
 add_lunch_combo rootbox_mako-userdebug
+add_lunch_combo rootbox_manta-userdebug
 add_lunch_combo rootbox_n7000-userdebug
 add_lunch_combo rootbox_n7100-userdebug
 add_lunch_combo rootbox_quincyatt-userdebug
