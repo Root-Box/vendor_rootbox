e0b341772af09f415244f45f3cbcff3a4c5126de
Gergely Szell (sethyx) <szell@hu.ibm.com>
2013-06-01 21:30:57 +0200

    Add Google Analytics support
    PS2: add branch build prop
    PS3: add device prop since in many cases the ro.product.device property differs
         from the AOKP codename
    
    Change-Id: Ica9f4034bf8363fb89bc40d3beb134c5aa17803d
    Signed-off-by: Gergely Szell (sethyx) <szell@hu.ibm.com>

--- configs/common_versions.mk
+++ configs/common_versions.mk
@@ -2,6 +2,7 @@
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_VERSION_TAGS=release-keys USER=android-build BUILD_UTC_DATE=$(shell date +"%s")
 
 DATE = $(shell date +%Y%m%d)
+RB_BRANCH=jb-mr1
 
 ifneq ($(RB_BUILD),)
 # RootBox Official properties
@@ -19,6 +20,10 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.rootbox.version=RootBox-JB-$(TARGET_PRODUCT)-Nightly-$(DATE)
 endif
 
+# needed for statistics
+PRODUCT_PROPERTY_OVERRIDES += \
+        ro.aokp.branch=$(RB_BRANCH) \
+        ro.aokp.device=$(RB_PRODUCT)
 
 # Camera shutter sound property
 PRODUCT_PROPERTY_OVERRIDES += \
--- prebuilt/Android.mk
+++ prebuilt/Android.mk
@@ -100,4 +100,10 @@ LOCAL_MODULE_SUFFIX := .apk
 include $(BUILD_PREBUILT)
 endif
 
-
+include $(CLEAR_VARS)
+LOCAL_MODULE := libGoogleAnalyticsV2
+LOCAL_SRC_FILES := common/lib/$(LOCAL_MODULE).jar
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_SUFFIX := .jar
+LOCAL_MODULE_CLASS := JAVA_LIBRARIES
+include $(BUILD_PREBUILT)
