fe3d3e78c72415a126783252d51feee6a20513eb
Bajee11 <bajee11@gmail.com>
2015-05-17 22:34:57 -0400

    Changes for Lollipop (1/2)

--- configs/common_versions.mk
+++ configs/common_versions.mk
@@ -2,35 +2,29 @@
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_VERSION_TAGS=release-keys USER=android-build BUILD_UTC_DATE=$(shell date +"%s")
 
 DATE = $(shell date +%Y%m%d)
-RB_BRANCH=jb-mr1
+RB_BRANCH=lollipop
 
 ifneq ($(RB_BUILD),)
     # RootBox Official properties
     PRODUCT_PROPERTY_OVERRIDES += \
-        ro.goo.developerid=Bajee \
-        ro.goo.rom=RootBox \
-        ro.goo.version=$(DATE) \
         ro.rootbox.build=$(DATE) \
-        ro.rootbox.version=RootBox-JB-$(TARGET_PRODUCT)-$(RB_BUILD)
+        ro.rootbox.version=RootBox-$(TARGET_PRODUCT)-$(RB_BUILD)
 else
     ifneq ($(RB_NIGHTLY),)
     # RootBox Nightlies properties
     PRODUCT_PROPERTY_OVERRIDES += \
-        ro.goo.developerid=Bajee \
-        ro.goo.rom=RootBoxNightlies \
-        ro.goo.version=$(DATE) \
         ro.rootbox.build=$(DATE) \
-        ro.rootbox.version=RootBox-JB-$(TARGET_PRODUCT)-Nightly-$(DATE)
+        ro.rootbox.version=RootBox-$(TARGET_PRODUCT)-Nightly-$(DATE)
     else
     PRODUCT_PROPERTY_OVERRIDES += \
-        ro.rootbox.version=RootBox-JB-$(TARGET_PRODUCT)-Unofficial-$(DATE)
+        ro.rootbox.version=RootBox-$(TARGET_PRODUCT)-Unofficial-$(DATE)
     endif
 endif
 
 # needed for statistics
 PRODUCT_PROPERTY_OVERRIDES += \
         ro.rootbox.branch=$(RB_BRANCH) \
-        ro.rootbox.device=$(RB_PRODUCT)
+        ro.rootbox.device=$(RB_DEVICE)
 
 # Camera shutter sound property
 PRODUCT_PROPERTY_OVERRIDES += \
